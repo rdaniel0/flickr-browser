9136e995066293e005f8756dc5b6e588261eb1d3
Daniel Rablin <rablin.daniel@gmail.com>
2021-03-13 10:08:03 +1000

    Initialize project using Create React App

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    font-family: Verdana, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  #root {
+    min-height: 100%;
+    min-width: 100%;
+  }
+
+  p,
+  label {
+    font-family: 'Barlow', 'Verdana', Times, 'Times New Roman', serif;
+    line-height: 1.5em;
+  }
+
+  input, select {
+    font-family: inherit;
+    font-size: inherit;
+  }
+`;
