31abc61cf5649aff0c0e0d5a6e230674b94e9c5d
Daniel Rablin <rablin.daniel@gmail.com>
2021-03-14 16:28:10 +1000

    Add public proxy for cors issue.

--- src/services/FlickrApi/FlickrApi.ts
+++ src/services/FlickrApi/FlickrApi.ts
@@ -2,6 +2,11 @@ import { ApiResponse } from './Models';
 
 const urlPublicPhotos = `https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1`;
 
+// Borrowing public proxy to avoid CORS on client browser, since Flickr server doesn't send CORS headers from the feeds endpoint.
+// In a real environment, you should deploy your own proxy server.
+const GET = (url: string) =>
+  fetch('https://cors-anywhere.herokuapp.com/' + url);
+
 const FlickrApi = {
   getPhotoFeed: (tags: string) => {
     // Construct url from passed in tags
@@ -10,7 +15,7 @@ const FlickrApi = {
       // And replace spaces with commas
       url += `&tags=${tags.replaceAll(' ', ',')}`;
     }
-    return fetch(url).then(response => {
+    return GET(url).then(response => {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
