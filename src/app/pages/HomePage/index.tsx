d733ac093a17541b7272af9ac5db4b5bffc5e6ff
Daniel Rablin <rablin.daniel@gmail.com>
2021-03-14 16:42:29 +1000

    Set up app state. Add api call to saga.

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { useSearchSlice } from './slice';
+import {
+  selectError,
+  selectLoading,
+  selectResults,
+  selectSearchText,
+} from './slice/selectors';
 export function HomePage() {
+  const dispatch = useDispatch();
+
+  const { actions } = useSearchSlice();
+  const searchText = useSelector(selectSearchText);
+  const results = useSelector(selectResults);
+  const isLoading = useSelector(selectLoading);
+  const error = useSelector(selectError);
   return (
     <>
       <Helmet>
